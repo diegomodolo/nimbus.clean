6ce1c002ee51c08b6588330d706a30f145c6aa26
Diego M. Ribeiro <diego.ribeiro@nectarnet.com.br>
2024-06-20 15:19:32 -0300

    CustomFieldName

---
+++ src/Modules/Nimbus.Modules.Infrastructure/Database/Custom.cs
@@ -0,0 +1,77 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="Custom.cs" company="Néctar Informática Ltda">
+//    Nimbus
+//  </copyright>
+//  <summary>
+//    Defines the Custom.cs type.
+//  </summary>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace Nimbus.Modules.Infrastructure.Database;
+
+using System.Globalization;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+public class CustomFieldNameConvention : IModelFinalizingConvention
+{
+    #region Public Methods and Operators
+
+    public void ProcessModelFinalizing(
+        IConventionModelBuilder modelBuilder,
+        IConventionContext<IConventionModelBuilder> context)
+    {
+        foreach (var entityType in modelBuilder.Metadata.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties())
+            {
+                if (property.Name.Length < 5)
+                {
+                    continue;
+                }
+
+                var backingFieldName = ConvertToBackingFieldName(property.Name);
+
+                var fieldInfo = entityType.ClrType.GetField(
+                    backingFieldName,
+                    System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+
+                if (fieldInfo != null)
+                {
+                    Console.WriteLine($"Setting backing field: {backingFieldName} for property: {property.Name}");
+
+                    modelBuilder.Entity(entityType.ClrType)
+                                .Property(property.ClrType, property.Name)
+                                .HasField(backingFieldName);
+                    // property.SetField(backingFieldName);
+                }
+            }
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    private static string ConvertToBackingFieldName(string propertyName)
+    {
+        if (string.IsNullOrEmpty(propertyName))
+        {
+            return propertyName;
+        }
+
+        // Split the property name at each underscore
+        var parts = propertyName.Split('_');
+
+        // Convert the first part to lowercase
+        var firstPartLower = parts[0].ToLower(CultureInfo.InvariantCulture);
+
+        // Convert the remaining parts to camelCase
+        var camelCaseParts = parts.Skip(1).Select(p => p).ToArray();
+
+        // Concatenate all parts and return
+        return $"_{firstPartLower}{string.Concat(camelCaseParts)}";
+    }
+
+    #endregion
+}
--- src/Modules/Nimbus.Modules.Infrastructure/Database/NimbusDbContext.cs
+++ src/Modules/Nimbus.Modules.Infrastructure/Database/NimbusDbContext.cs
@@ -10,18 +10,64 @@
 namespace Nimbus.Modules.Infrastructure.Database;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Nimbus.Common.Domain.Abstractions;
 using Nimbus.Common.Domain.Cadastros.EstruturaOrganizacional.C001;
 using Nimbus.Common.Domain.Cadastros.Sistema;
+using Nimbus.Modules.Infrastructure.Database.Migrations.CompiledModel;
 
-public sealed class NimbusDbContext(DbContextOptions<NimbusDbContext> options) : DbContext(options),
-    IUnitOfWork
+public sealed class NimbusDbContext : DbContext,
+                                      IUnitOfWork
 {
+    #region Constructors and Destructors
+
+    // public NimbusDbContext(DbContextOptions<NimbusDbContext> options)
+    //     : base(options)
+    // {
+    // }
+
+    public NimbusDbContext()
+    {
+    }
+
+    #endregion
+
     #region Public Properties
 
     public DbSet<C001_Holding> C001_Holding { get; set; }
 
     public DbSet<S008_Usuario> S008_Usuario { get; set; }
+
+    #endregion
+
+    #region Methods
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.ConfigureWarnings(warn => warn.Ignore(CoreEventId.DetachedLazyLoadingWarning));
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(
+                $"Data Source=192.168.0.8;Initial Catalog=NimbusDbProducaoGV;User Id=NimbusBaseUser;Pwd=Senha;MultipleActiveResultSets=True;Max Pool Size=300;Connection Timeout=0;TrustServerCertificate=true;");
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        optionsBuilder.UseModel(NimbusDbContextModel.Instance);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+        configurationBuilder.Conventions.Add(_ => new CustomFieldNameConvention());
+    }
+
+    /// <inheritdoc />
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder.UseSqlServer("Server=.;Database=Nimbus;Trusted_Connection=True;");
+    // }
 
     #endregion
 }
